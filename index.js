20735a5836f0a60051b0da1c878ffafd37c15c3d
Martin Visuara <martinvisuara10@gmail.com>
2025-10-17 21:18:23 -0300

    ejercicio terminado

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista");
+  const itemsEls = document.querySelectorAll(".lista li");
+
+  for (let el of itemsEls) {
+    el.remove();
+  }
+
+  for (let item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = item.tema;
+    const laClaseQueLeVoyAAsignarAlli = item.class || "item";
+
+    newLiEl.classList.add(laClaseQueLeVoyAAsignarAlli);
+
+    listaEl.appendChild(newLiEl);
+  }
+}
 
 main();
